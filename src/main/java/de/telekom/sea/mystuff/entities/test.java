02dc40491f53d54e8b5d2e037895010d7d7a2554
Robert Duschek <robert.duschek@telekom.de>
2020-05-20 17:39:52 +0200

    String Output on /api/v1/items

---
+++ src/main/java/de/telekom/sea/mystuff/entities/test.java
@@ -0,0 +1,12 @@
+package de.telekom.sea.mystuff.entities;
+
+public class test {
+	
+	private Long id;
+	private String name;
+	private int amount;
+	private String location;
+	private String description;
+	
+
+}
