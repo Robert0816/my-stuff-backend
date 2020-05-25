b72a21199508277006a8f782322b6d261ba53ab2
Robert Duschek <robert.duschek@telekom.de>
2020-05-25 07:22:24 +0200

    add crud

--- src/main/java/de/telekom/sea/mystuff/backend/controller/MyStuffRestController.java
+++ src/main/java/de/telekom/sea/mystuff/backend/controller/MyStuffRestController.java
@@ -2,10 +2,20 @@ package de.telekom.sea.mystuff.backend.controller;
 
 import java.util.List;
 
+import javax.management.relation.RelationNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import de.telekom.sea.mystuff.backend.entities.Item;
 import de.telekom.sea.mystuff.backend.repo.MystuffRepo;
@@ -14,10 +24,10 @@ import de.telekom.sea.mystuff.backend.repo.MystuffRepo;
 @RestController
 public class MyStuffRestController {
 
-		@RequestMapping("/items")
-		String test() {
-			return "Hello World!!";
-		}
+	@RequestMapping("/items")
+	String test() {
+		return "Hello World!!";
+	}
 
 	private MystuffRepo mystuffRepo;
 
@@ -31,7 +41,26 @@ public class MyStuffRestController {
 	public List<Item> getAllItems() {
 		return this.mystuffRepo.findAll();
 	}
-	
-	
+
+	@GetMapping("{id}")
+	public Item get(@PathVariable Long id) {
+		return mystuffRepo.findById(id).orElseThrow(() -> {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		});
+	}
+
+	@PostMapping("/items")
+	public Item createItem(@Validated @RequestBody Item newItem) {
+		return mystuffRepo.save(newItem);
+	}
+
+	@DeleteMapping("Item/{id}")
+	public void delete(@PathVariable Long id) throws RelationNotFoundException {
+		try {
+			mystuffRepo.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new RelationNotFoundException();
+		}
+	}
 
 }
